36e9c8a0c1bc6d4ed8ab3e75203466725b01ee93
zhenyuwei99 <zhenyuwei99@gmail.com>
2021-03-15 23:49:11 +0800

    Add test for script package

---
+++ pdff_script/tests/test_scriptEqNVT.py
@@ -0,0 +1,25 @@
+import pytest, os
+from .. import ScriptEqNVT
+
+cur_dir = os.path.abspath(os.path.dirname(os.path.abspath(__file__)))
+forcefield_dir = os.path.join(cur_dir, '../forcefield/amberff14sb')
+
+class TestScriptEqNVT:
+    def setup(self):
+        self.script = ScriptEqNVT(
+            save_dir=os.path.join(cur_dir, 'output'), 
+            forcefield_file=os.path.join(forcefield_dir, 'protein.ff14SB_nonbonded.xml'),
+            model_name='alpha helix validation', cuda_id=0
+        )
+
+    def teardown(self):
+        self.script = None
+
+    def test_attributes(self):
+        pass
+
+    def test_exceptions(self):
+        pass
+
+    def test_writeFile(self):
+        self.script.writeFile()
