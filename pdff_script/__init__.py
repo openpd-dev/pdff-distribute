43b71f2916d45c9ab0cc5f3c0ce537a2c6440e98
zhenyuwei99 <zhenyuwei99@gmail.com>
2021-03-16 17:08:12 +0800

    Add MASS_DICT BACK_BONE_ATOMS and peptide list

--- pdff_script/__init__.py
+++ pdff_script/__init__.py
@@ -4,16 +4,58 @@ __email__ = "zhenyuwei99@gmail.com"
 __copyright__ = "Copyright 2021-2021, Southeast University and Zhenyu Wei"
 __license__ = "GPLv3"
 
-ELEMENT_MASS = {
-    'H': 1.00800,
-    'C': 12.01100, 
-    'N': 14.00700,
-    'O': 15.9990,
-    'S': 32.06000
+MASS_DICT = {
+    "C": 12,
+    "H": 1,
+    "O": 16,
+    "N": 14,
+    "S": 32
 }
 
-from pdff_script.pdbManipulator import PDBManipulator
-
-__all__ = [
-    'PDBManipulator'
+BACK_BONE_ATOMS = [
+    "C", "O", "OXT",
+    "CA", "HA", "HA2",
+    "N", "H1", "H2", "H3", "H"
 ]
+
+TRIPLE_LETTER_ABBREVIATION = [
+    'ALA', 'ARG', 'ASN', 'ASP',
+    'CYS', 'GLN', 'GLU', 'GLY',
+    'HIS', 'ILE', 'LEU', 'LYS',
+    'MET', 'PHE', 'PRO', 'SER',
+    'THR', 'TRP', 'TYR', 'VAL'
+]
+
+SINGLE_LETTER_ABBREVIATION = [
+    'A', 'R', 'N', 'D',
+    'C', 'Q', 'E', 'G',
+    'H', 'I', 'L', 'K',
+    'M', 'F', 'P', 'S',
+    'T', 'W', 'Y', 'V'
+]
+
+def isStandardPeptide(*peptides):
+    for peptide in peptides:
+        peptide = peptide.upper()
+        if len(peptide) == 3:
+            if not peptide in TRIPLE_LETTER_ABBREVIATION:
+                raise ValueError(
+                    'Peptide type %s is not in the standard peptide list:\n %s' 
+                    %(peptide, TRIPLE_LETTER_ABBREVIATION)
+                )
+        elif len(peptide) == 1:
+            if not peptide in SINGLE_LETTER_ABBREVIATION:
+                raise ValueError(
+                    'Peptide type %s is not in the standard peptide list:\n %s' 
+                    %(peptide, SINGLE_LETTER_ABBREVIATION)
+                )
+        else:
+            raise ValueError(
+                'Peptide type should be 1 or 3 letters, instead of %d letters'
+                %(len(peptide))
+            )
+    return True
+    
+from pdff_script.pdbManipulator import PDBManipulator
+from pdff_script.script import *
+from pdff_script.recipe import *
