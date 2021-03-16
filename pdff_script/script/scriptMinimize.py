2eda987bc5f052a35e92624e3046ffdc5a79430b
zhenyuwei99 <zhenyuwei99@gmail.com>
2021-03-16 17:23:22 +0800

    Update Sciprt Class

--- pdff_script/script/scriptMinimize.py
+++ pdff_script/script/scriptMinimize.py
@@ -84,7 +84,7 @@ class ScriptMinimize(Script):
         self, save_dir: str, model_name: str, forcefield_file: str, cuda_id: int,
         file_name='01_min.py'
     ) -> None:
-        super().__init__(save_dir, model_name, file_name=file_name)
+        super().__init__(save_dir, model_name, file_name)
         self.forcefield_file = forcefield_file
         self.cuda_id = cuda_id
     
