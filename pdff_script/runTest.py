cd11d23e3017f6dab9feb08ba58213c319834711
zhenyuwei99 <zhenyuwei99@gmail.com>
2021-03-17 12:33:31 +0800

    Add header to pdbManipulator.py and runTest.py

--- pdff_script/runTest.py
+++ pdff_script/runTest.py
@@ -1,4 +1,16 @@
-import pytest, os, argparse
+#!/usr/bin/env python
+# -*-coding:utf-8 -*-
+'''
+file: runTest.py
+created time : 2021/03/14
+last edit time : 2021/03/17
+author : Zhenyu Wei 
+version : 1.0
+contact : zhenyuwei99@gmail.com
+copyright : (C)Copyright 2021-2021, Zhenyu Wei and Southeast University
+'''
+
+import pytest, os
 cur_dir = os.path.abspath(os.path.dirname(os.path.abspath(__file__)))
 test_dir = os.path.join(cur_dir, 'tests')
 
